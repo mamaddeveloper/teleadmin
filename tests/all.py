c0621c84896bf049341b7a0488f83485d2ae537b
fa18swiss <fa18swiss@users.noreply.github.com>
2015-07-03 15:23:41 +0200

    remove unused file

--- tests/all.py
+++
@@ -1,8 +0,0 @@
-from tests.testUserList import TestUserList
-from tests.testUserCount import TestUserCount
-from tests.testLines import TestLines
-from tests.testBad import TestBad
-import unittest
-
-if __name__ == '__main__':
-    unittest.main()
