f63cfecaa3a782b8682a95c6697ee2f4abdb8fd6
fa18swiss <fa18swiss@users.noreply.github.com>
2015-07-03 14:06:55 +0200

    add ModuleVersion

---
+++ modules/mod_version.py
@@ -0,0 +1,15 @@
+from modules.module_base import ModuleBase
+import os.path
+import subprocess
+
+class ModuleVersion(ModuleBase):
+    PATH = os.path.dirname(os.path.dirname(__file__))
+
+    def notify_command(self, message_id, from_attr, date, chat, commandName, commandStr):
+        if commandName == "version":
+            try:
+                text = subprocess.check_output(["git","log", "-1"], cwd=self.PATH).decode("utf-8")
+                print(type(text))
+                self.bot.sendMessage(text, chat["id"])
+            except:
+                self.bot.sendMessage("Error getting version", chat["id"])
