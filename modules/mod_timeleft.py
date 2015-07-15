a8a1aa04683abdc699f235518817d16e10b3cebe
fa18swiss <fa18swiss@users.noreply.github.com>
2015-07-15 11:51:04 +0200

    update timeleft
    fix #1
    fix budapest date

--- modules/mod_timeleft.py
+++ modules/mod_timeleft.py
@@ -3,7 +3,7 @@ from datetime import datetime
 class ModuleTimeleft(ModuleBase):
     DATES = {
         "rendu": datetime(2015, 7, 14, 17, 00),
-        "budapest": datetime(2015, 7, 15, 14, 00),
+        "budapest": datetime(2015, 7, 16, 14, 00),
         "défences": datetime(2015, 8, 17, 00, 00),
         "diplome": datetime(2015, 10, 19, 18, 00),
     }
@@ -14,14 +14,26 @@ class ModuleTimeleft(ModuleBase):
 
     def notify_command(self, message_id, from_attr, date, chat, commandName, commandStr):
         if commandName == "timeleft":
-            if commandStr in self.DATES:
+            if commandStr == "all":
+                for date in self.DATES:
+                    diff = self.DATES[date] - datetime.today()
+                    text = "Il reste %s jusqu'à %s" % (diff, date)
+                    self.bot.sendMessage(text, chat["id"])
+                return
+            elif commandStr in self.DATES:
                 diff = self.DATES[commandStr] - datetime.today()
                 text = "Il reste %s jusqu'à %s" % (diff, commandStr)
             else:
                 text = "Echéance %s inconnue" % commandStr
             self.bot.sendMessage(text, chat["id"])
         elif commandName == "date":
-            if commandStr in self.DATES:
+            if commandStr == "all":
+                for d in self.DATES:
+                    date = self.DATES[d]
+                    text = "Date %s : %s" % (d, date)
+                    self.bot.sendMessage(text, chat["id"])
+                return
+            elif commandStr in self.DATES:
                 date = self.DATES[commandStr]
                 text = "Date %s : %s" % (commandStr, date)
             else:
