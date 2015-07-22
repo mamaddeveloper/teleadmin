a2c6d4c8402fcca9e4324939c0babb3f30815f0a
fa18swiss <fa18swiss@users.noreply.github.com>
2015-07-22 12:19:00 +0200

    add module best sentences

---
+++ modules/mod_best_sentences.py
@@ -0,0 +1,30 @@
+from modules.module_base import ModuleBase
+from tools.bestSentences import *
+import threading
+
+class ModuleDtc(ModuleBase):
+
+    def __init__(self, bot):
+        ModuleBase.__init__(self, bot)
+        self.name = "BestSentences"
+        self.best_sentences = BestSentences()
+        threading.Thread(target=self.best_sentences.load).start()
+
+    def notify_command(self, message_id, from_attr, date, chat, commandName, commandStr):
+        if commandName == "bs":
+            if commandStr == "load":
+                if self.bot.admin.is_admin(from_attr):
+                    self.best_sentences.load()
+                    self.bot.sendMessage("Reloaded !", chat["id"])
+                else:
+                    self.bot.sendMessage("Access denied !", chat["id"])
+            else:
+                try:
+                    s = self.best_sentences.get(commandStr)
+                    text = "%s\n\n%s, %s" % (s["text"], s["who"], s["date"])
+                    self.bot.sendMessage(text, chat["id"])
+                except BestSentencesNotFoundException:
+                    self.bot.sendMessage("User %s not found !" % commandStr, chat["id"])
+
+                except BestSentencesNotLoadedException:
+                    self.bot.sendMessage("Module not loaded !", chat["id"])
