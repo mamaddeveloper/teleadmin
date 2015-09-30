f5bf186c32bc5c05bdffb3b3a98bb5c9d3254b77
fa18swiss <fa18swiss@users.noreply.github.com>
2015-09-30 19:37:01 +0200

    add list modules loaded

---
+++ modules/mod_list_modules.py
@@ -0,0 +1,21 @@
+from modules.module_base import ModuleBase
+import inspect
+
+class ModuleListModule(ModuleBase):
+    def __init__(self, bot):
+        ModuleBase.__init__(self, bot)
+        self.name = "ModuleListModule"
+
+
+    def notify_command(self, message_id, from_attr, date, chat, commandName, commandStr):
+        if commandName == "modules":
+            if (self.bot.admin.is_admin(from_attr)):
+                print("\r\n".join(["%s : %s" % (m.name, type(m)) for m in self.bot.listModules]))
+                self.bot.sendMessage("\r\n".join(["%s in %s" % (m.name, inspect.getfile(m.__class__)) for m in self.bot.listModules]), chat["id"])
+            else:
+                self.bot.sendMessage("Admins only", chat["id"])
+
+    def get_commands(self):
+        return [
+            ("modules", "List loaded modules (admin only)"),
+        ]
