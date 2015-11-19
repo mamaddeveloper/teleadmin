7741b3ada6975d2ba12f29f51327f7e654597b91
fa18swiss <fa18swiss@users.noreply.github.com>
2015-11-19 09:51:03 +0100

    add more log

--- modules/mod_random.py
+++ modules/mod_random.py
@@ -62,7 +62,7 @@ class ImageSenderTask(Thread):
     def run(self):
         self.logger.info("Starting")
         for i in range(self.n):
-            self.logger.debug("Start %d", i)
+            self.logger.info("Start %d", i)
             image_type = random.choice(self.images_type)
             result = self.nsfw.image(image_type, "random")
             if result.ok():
